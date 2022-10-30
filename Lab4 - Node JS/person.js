7d0f1b0d12909bd03061022ebd0e9fbd1e7e319e
Wilson <wilsontancher@gmail.com>
2022-10-30 17:43:24 +0800

    Ex on DOM and Lab 4 Added

---
+++ Lab4 - Node JS/person.js
@@ -0,0 +1,18 @@
+var person= {
+    name: "",
+    age: 0,
+    setName: function(n) {
+        this.name=n;
+    },
+    getName: function() {
+        return this.name;
+    },
+    setAge: function(a)  {
+        this.age=a;
+    },
+    getAge: function(){
+        return this.age;
+    }
+};
+
+module.exports = person;
